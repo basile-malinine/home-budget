7be4663bc82ef395052a76704362ca91adc6eb4d
Basile <basile-m@yandex.ru>
2025-08-05 06:11:06 +0300

    Изменена миграция create_user_table.

--- migrations/m241210_074641_create_user_table.php
+++ migrations/m241210_074641_create_user_table.php
@@ -2,14 +2,8 @@
 
 use yii\db\Migration;
 
-/**
- * Class m241210_074641_create_table_user
- */
-class m241210_074641_create_table_user extends Migration
+class m241210_074641_create_user_table extends Migration
 {
-    /**
-     * {@inheritdoc}
-     */
     public function up()
     {
         $this->createTable('user', [
@@ -17,12 +11,16 @@ class m241210_074641_create_table_user extends Migration
             'name' => $this->string()->notNull()->comment('Имя пользователя'),
             'email' => $this->string()->notNull()->unique()->comment('E-mail'),
             'pass_hash' => $this->string()->notNull()->comment('Пароль'),
+            'active' => $this->smallInteger(1)->notNull()->defaultValue(1)->comment('Статус'),
+        ]);
+
+        $this->insert('user', [
+            'name' => 'admin',
+            'email' => 'admin@mail.net',
+            'pass_hash' => Yii::$app->getSecurity()->generatePasswordHash('admin'),
         ]);
     }
 
-    /**
-     * {@inheritdoc}
-     */
     public function down()
     {
         $this->dropTable('user');
