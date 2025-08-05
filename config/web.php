d0c6323e1b49ecc0255790606b421e6be8aa3b0d
Basile <basile-m@yandex.ru>
2025-08-05 08:43:56 +0300

    Добавлен шаблон модальной формы. Заменён логотип.

--- config/web.php
+++ config/web.php
@@ -5,7 +5,7 @@ $db = require __DIR__ . '/db.php';
 
 $config = [
     'id' => 'basic',
-    'name' => 'АПК-ТЕХНОЛОГИИ',
+    'name' => 'Home budget',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'language' => 'ru-RU',
